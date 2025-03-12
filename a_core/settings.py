e4f34545e863bf88217f2f6020fa3ce2f0d01fa4
INNOVATIONKSA <mohdadnansameer@gmail.com>
2025-03-13 00:04:59 +0530

    Let's go

--- a_core/settings.py
+++ a_core/settings.py
@@ -36,14 +36,14 @@ if ENVIRONMENT == 'development':
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'lume-wqxk.onrender.com']
 
 INTERNAL_IPS = (
       '127.0.0.1',
       'localhost:8000'
 )
 
-CSRF_TRUSTED_ORIGINS = [ 'https://*' ]
+CSRF_TRUSTED_ORIGINS = [ 'https://lume-wqxk.onrender.com' ]
 
 
 # Application definition
